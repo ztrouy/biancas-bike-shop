c7fdff775369e122446df516a104913f32966877
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-17 14:50:11 -0500

    Added fetch for updating a Work Order

--- client/src/managers/workOrderManager.js
+++ client/src/managers/workOrderManager.js
@@ -15,3 +15,15 @@ export const createWorkOrder = (workOrder) => {
 
     return fetch(_apiUrl, postOptions).then(res => res.json())
 }
+
+export const updateWorkOrder = (workOrder) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(workOrder)
+    }
+
+    return fetch(`${_apiUrl}/${workOrder.id}`, putOptions)
+}
