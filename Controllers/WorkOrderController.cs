b40a34800c12e0dc347f0e4dd10b89582f9c2ba9
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-17 14:49:22 -0500

    Added PUT endpoint that updates a Work Order

--- Controllers/WorkOrderController.cs
+++ Controllers/WorkOrderController.cs
@@ -46,4 +46,28 @@ public class WorkOrderController : ControllerBase
 
         return Created($"/api/workorder/{workOrder.Id}", workOrder);
     }
+
+    [HttpPut("{id}")]
+    [Authorize]
+    public IActionResult UpdateWorkOrder(WorkOrder workOrder, int id)
+    {
+        WorkOrder workOrderToUpdate = _dbContext.WorkOrders.SingleOrDefault(wo => wo.Id == id);
+        if (workOrderToUpdate == null)
+        {
+            return NotFound();
+        }
+        else if (id != workOrder.Id)
+        {
+            return BadRequest();
+        }
+
+        // These are the only properties that we want to make editable
+        workOrderToUpdate.Description = workOrder.Description;
+        workOrderToUpdate.UserProfileId = workOrder.UserProfileId;
+        workOrderToUpdate.BikeId = workOrder.BikeId;
+
+        _dbContext.SaveChanges();
+
+        return NoContent();
+    }
 }
