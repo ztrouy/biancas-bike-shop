e7996864464654af239e690c731b83513d20e158
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-17 14:09:09 -0500

    Added fetch for creating a Work Order

--- client/src/managers/workOrderManager.js
+++ client/src/managers/workOrderManager.js
@@ -3,3 +3,15 @@ const _apiUrl = "/api/workorder"
 export const getIncompleteWorkOrders = () => {
     return fetch(`${_apiUrl}/incomplete`).then(res => res.json())
 }
+
+export const createWorkOrder = (workOrder) => {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(workOrder)
+    }
+
+    return fetch(_apiUrl, postOptions).then(res => res.json())
+}
